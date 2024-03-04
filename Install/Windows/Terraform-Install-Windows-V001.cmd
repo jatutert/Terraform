2ee83d2307655b3ee72447b3dc8917dae7942bec
John Tutert <jatutert@gmail.com>
2024-03-04 20:52:29 +0100

    Update

---
+++ Install/Windows/Terraform-Install-Windows-V001.cmd
@@ -0,0 +1,8 @@
+::
+:: Terraform Installatie Windows 
+:: John Tutert
+:: 29 februari 2024
+:: 
+winget install --id Hashicorp.Terraform
+:: Afsluiten Shell om Terraform te laden 
+Exit 0
---
+++ VMWare-Workstation/Install-VMW-WrkStat-Provider-V001.cmd
@@ -0,0 +1,3 @@
+curl -o %APPDATA%\terraform.rc [github url]
+curl -o %USERPROFILE%\main.tf [github.url
+terraform init
